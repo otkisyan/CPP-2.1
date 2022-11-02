c9e61d3380722d991497f5b754fa315d7fda2482
01101111 01110100 01101011 01101001 01110011 01111001 01100001 01101110 <77882873+otkisyan@users.noreply.github.com>
2022-11-02 08:36:47 +0100

    Update Обход вершин дерева.cpp

--- Обход вершин дерева.cpp
+++ Обход вершин дерева.cpp
@@ -1,6 +1,7 @@
 // https://acm.bsu.by/wiki/%D0%91%D0%B8%D0%BD%D0%B0%D1%80%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE%D0%B8%D1%81%D0%BA%D0%BE%D0%B2%D1%8B%D0%B5_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D1%8C%D1%8F#:~:text=%D0%9F%D1%80%D1%8F%D0%BC%D0%BE%D0%B9%20%D0%BF%D0%BE%D1%80%D1%8F%D0%B4%D0%BE%D0%BA%20%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%B0%20(%D1%81%D0%B2%D0%B5%D1%80%D1%85%D1%83%20%D0%B2%D0%BD%D0%B8%D0%B7,%D0%BF%D1%80%D1%8F%D0%BC%D1%8B%D0%BC%20%D0%BB%D0%B5%D0%B2%D1%8B%D0%BC%20(%D0%BF%D1%80%D0%B0%D0%B2%D1%8B%D0%BC)%20%D0%BE%D0%B1%D1%85%D0%BE%D0%B4%D0%BE%D0%BC.
 // https://prog-cpp.ru/data-tree/
 // http://hci.fenster.name/304y/lab3/
+// https://learnc.info/adt/binary_tree_traversal.html
 // https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 
 /*
